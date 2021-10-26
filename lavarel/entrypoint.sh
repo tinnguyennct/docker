1c50f48f396343b06e71bbd3d63e658470a3c465
tinnguyennct <chanhtinvt54@gmail.com>
2021-10-26 15:50:24 +0700

    Add file

--- lavarel/entrypoint.sh
+++ lavarel/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Run
-cp /apache_exporter-*.linux-amd64/apache_exporter /usr/local/bin
-chmod +x /usr/local/bin/apache_exporter
-groupadd --system prometheus
-useradd -s /sbin/nologin --system -g prometheus prometheus
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
 #Start services
 service apache2 start
-service apache_exporter.service start
+service supervisor start
 exec "$@"
